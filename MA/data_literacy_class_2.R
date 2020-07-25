821390b1720f8eed074531dcc18c552b6f450bea
paulapivat <paul.apivat@gmail.com>
2020-07-25 19:08:36 +0700

    add data_literacy_class_2.R

---
+++ MA/data_literacy_class_2.R
@@ -0,0 +1,39 @@
+# Session Info ----
+
+sessionInfo()  # command
+
+R version 3.6.3 (2020-02-29)
+Platform: x86_64-apple-darwin15.6.0 (64-bit)
+Running under: macOS Catalina 10.15.5
+
+── Attaching packages ────────────────────────────────────────────────────────── tidyverse 1.3.0 ──
+✓ ggplot2 3.3.1     ✓ purrr   0.3.3
+✓ tibble  3.0.1     ✓ dplyr   0.8.5
+✓ tidyr   1.0.0     ✓ stringr 1.4.0
+✓ readr   1.3.1     ✓ forcats 0.4.0
+
+# Libraries & Packages ----
+library(tidyverse)
+
+# Loading Data ----
+
+# data source: https://unstats.un.org/sdgs/indicators/database/
+# select Goal 4 2000-2019,
+
+
+
+# Cleaning Data ---- 
+
+# Outliers and Missing Data ----
+
+# Manipulating Columns and Rows ----
+
+# str(), glimpse()
+
+# Plot: Boxplot
+
+# Plot: Histogram / Density Plots
+
+
+
+
